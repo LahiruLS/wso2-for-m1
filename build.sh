568209cd69f0e4467dc1892d9423e395476d54d6
LahiruLS <lahiru.lls@gmail.com>
2021-11-01 21:13:01 +0530

    Add simple build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+docker build -t lahirulls/wso2is-for-m1:5.11.0-centos .
+docker push lahirulls/wso2is-for-m1:5.11.0-vanilla-centos
+
+docker build -t lahirulls/wso2is-for-m1:5.11.0-ubuntu .
+docker push lahirulls/wso2is-for-m1:5.11.0-vanilla-ubuntu
+
+docker build -t lahirulls/wso2is-for-m1:5.11.0-centos .
+docker push lahirulls/wso2is-for-m1:5.11.0-centos
+
+docker build -t lahirulls/wso2is-for-m1:5.11.0-ubuntu .
+docker push lahirulls/wso2is-for-m1:5.11.0-ubuntu
+
+docker build -t lahirulls/wso2apim-for-m1:4.0.0-centos .
+docker push lahirulls/wso2apim-for-m1:4.0.0-vanilla-centos
+
+docker build -t lahirulls/wso2apim-for-m1:4.0.00-ubuntu .
+docker push lahirulls/wso2apim-for-m1:4.0.00-vanilla-ubuntu
+
+docker build -t lahirulls/wso2apim-for-m1:4.0.0-centos .
+docker push lahirulls/wso2apim-for-m1:4.0.0-centos
+
+docker build -t lahirulls/wso2apim-for-m1:4.0.0-ubuntu .
+docker push lahirulls/wso2apim-for-m1:4.0.0-ubuntu
