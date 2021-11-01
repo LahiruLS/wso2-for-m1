66b45e4b295bb1ec5a78cd6d65fcdfc4f10e5d40
LahiruLS <lahiru.lls@gmail.com>
2021-11-01 22:43:41 +0530

    fix build tags

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 cd ./dockerfiles/centos/is
-docker build -t lahirulls/wso2is-for-m1:5.11.0-vanilla-centos .
-docker push lahirulls/wso2is-for-m1:5.11.0-vanilla-centos
+docker build -t lahirulls/wso2is-for-m1:5.11.0-centos .
+docker push lahirulls/wso2is-for-m1:5.11.0-centos
 
 cd ./dockerfiles/ubuntu/is
-docker build -t lahirulls/wso2is-for-m1:5.11.0-vanilla-ubuntu .
-docker push lahirulls/wso2is-for-m1:5.11.0-vanilla-ubuntu
+docker build -t lahirulls/wso2is-for-m1:5.11.0-ubuntu .
+docker push lahirulls/wso2is-for-m1:5.11.0-ubuntu
 
 cd ./dockerfiles/centos/apim
-docker build -t lahirulls/wso2apim-for-m1:4.0.0-vanilla-centos .
-docker push lahirulls/wso2apim-for-m1:4.0.0-vanilla-centos
+docker build -t lahirulls/wso2apim-for-m1:4.0.0-centos .
+docker push lahirulls/wso2apim-for-m1:4.0.0-centos
 
 cd ./dockerfiles/ubuntu/apim
-docker build -t lahirulls/wso2apim-for-m1:4.0.0-vanilla-ubuntu .
-docker push lahirulls/wso2apim-for-m1:4.0.0-vanilla-ubuntu
+docker build -t lahirulls/wso2apim-for-m1:4.0.0-ubuntu .
+docker push lahirulls/wso2apim-for-m1:4.0.0-ubuntu
