230ad423b139d50cd1f99261c5ad59a6a368f076
LahiruLS <lahiru.lls@gmail.com>
2021-11-01 21:39:55 +0530

    Remove unncessory builds

--- build.sh
+++ build.sh
@@ -8,14 +8,6 @@ cd ./dockerfiles/ubuntu/is
 docker build -t lahirulls/wso2is-for-m1:5.11.0-vanilla-ubuntu .
 docker push lahirulls/wso2is-for-m1:5.11.0-vanilla-ubuntu
 
-cd ./dockerfiles/centos/is-for-subscribed
-docker build -t lahirulls/wso2is-for-m1:5.11.0-centos .
-docker push lahirulls/wso2is-for-m1:5.11.0-centos
-
-cd ./dockerfiles/ubuntu/is-for-subscribed
-docker build -t lahirulls/wso2is-for-m1:5.11.0-ubuntu .
-docker push lahirulls/wso2is-for-m1:5.11.0-ubuntu
-
 cd ./dockerfiles/centos/apim
 docker build -t lahirulls/wso2apim-for-m1:4.0.0-vanilla-centos .
 docker push lahirulls/wso2apim-for-m1:4.0.0-vanilla-centos
@@ -23,11 +15,3 @@ docker push lahirulls/wso2apim-for-m1:4.0.0-vanilla-centos
 cd ./dockerfiles/ubuntu/apim
 docker build -t lahirulls/wso2apim-for-m1:4.0.0-vanilla-ubuntu .
 docker push lahirulls/wso2apim-for-m1:4.0.0-vanilla-ubuntu
-
-cd ./dockerfiles/centos/apim-for-subscribed
-docker build -t lahirulls/wso2apim-for-m1:4.0.0-centos .
-docker push lahirulls/wso2apim-for-m1:4.0.0-centos
-
-cd ./dockerfiles/ubuntu/apim-for-subscribed
-docker build -t lahirulls/wso2apim-for-m1:4.0.0-ubuntu .
-docker push lahirulls/wso2apim-for-m1:4.0.0-ubuntu
